3c694585f4b5387784bb39a9f316a6067dcb43f6
Niklas Rang <rang.niklas@mm-bbs.de>
2024-08-15 13:18:46 +0200

    Initial Commit!

---
+++ webserver.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+import time
+import json
+
+data = {}
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<h1>Die Expertengruppe der Experten</h1>"
+
+@app.route("/create", methods=['POST'])
+def add_data():
+    received_data = request.args.get('data')
+    print(received_data)
+    data[str(time.ctime())] = received_data
+    save_json(data)
+
+    return data
+
+
+
+def read_json():
+    with open('data.json', 'r') as jsonfile:
+        data = json.load(jsonfile)
+    return data
+
+def save_json(data):
+    with open('data.json', 'w') as jsonfile:
+        json.dump(data, jsonfile)
+
+
+
+if __name__ == '__main__':
+    print("Server is running")
+    data = read_json()
+    app.run(debug=True)
+    
